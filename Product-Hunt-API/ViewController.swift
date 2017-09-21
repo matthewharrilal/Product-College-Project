181390603d32202616b3a25cfd79a241767e4259
matthewharrilal <matthewharrilal@gmail.com>
2017-09-20 21:53:53 -0700

    First commit to the Product Hunt project

---
+++ Product-Hunt-API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Product-Hunt-API
+//
+//  Created by Matthew Harrilal on 9/20/17.
+//  Copyright © 2017 Matthew Harrilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
